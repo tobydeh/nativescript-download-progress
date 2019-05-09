8ff8a9a54a81dd3d299f9b717643c6d0d122658c
Ayush Singh <ayush-_-singh@live.com>
2019-05-10 00:38:10 +0530

    Update typings
    * added myself to contributors in package.json
    * fix flavorDimension error on android
    * bump version number

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,15 @@
 import * as fs from "tns-core-modules/file-system";
 
+declare type RequestOptions = {
+  method: string;
+  headers: Object;
+};
 export declare class DownloadProgress {
-    constructor();
-    addProgressCallback(callback: any);
-    downloadFile(url: string, destinationFilePath?: string): Promise<fs.File>;
+  constructor();
+  addProgressCallback(callback: any);
+  downloadFile(
+    url: string,
+    options: RequestOptions,
+    destinationFilePath?: string
+  ): Promise<fs.File>;
 }
