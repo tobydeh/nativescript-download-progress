392a8d82f1a6ffe74c76a6911d0ca467382f8ee8
Toby de Havilland <tobydeh@gmail.com>
2019-06-08 08:17:20 +0100

    Added eslint config for android worker

--- src/android-worker.js
+++ src/android-worker.js
@@ -1,90 +1,93 @@
-if (global["TNS_WEBPACK"]) {
+/* global java org */
+if (global['TNS_WEBPACK']) {
   if (global.android) {
     // without this JavaProxy is missing and we can't import vendor below
-    global.require("~/../internal/ts_helpers.js");
+    global.require('~/../internal/ts_helpers.js')
   }
-  global.require("~/vendor");
+  global.require('~/vendor')
 } else {
-  require("globals");
+  require('globals')
 }
-//import * as fs from "tns-core-modules/file-system";
-//import * as common from "tns-core-modules/http/http-request/http-request-common";
-var fs = require("tns-core-modules/file-system");
-var common = require("tns-core-modules/http/http-request/http-request-common");
+const fs = require('tns-core-modules/file-system')
+const common = require('tns-core-modules/http/http-request/http-request-common')
 
-global.onmessage = function(msg) {
-  var url = msg.data.url;
-  var destinationFilePath = msg.data.destinationFilePath;
-  const options = msg.data.options;
-  var destinationFile;
+global.onmessage = function (msg) {
+  const url = msg.data.url
+  const destinationFilePath = msg.data.destinationFilePath
+  const options = msg.data.options
+  let destinationFile
+  let contentLength = 0
+  let input
+  let output
+  let connection
 
-  var contentLength = 0;
   try {
     if (destinationFilePath) {
-      destinationFile = fs.File.fromPath(destinationFilePath);
+      destinationFile = fs.File.fromPath(destinationFilePath)
     } else {
-      destinationFile = fs.File.fromPath(common.getFilenameFromUrl(url));
+      destinationFile = fs.File.fromPath(common.getFilenameFromUrl(url))
     }
-    destinationFile.writeTextSync("", function(e) {
-      throw e;
-    });
-    var javaOptions = new org.nativescript.widgets.Async.Http.RequestOptions();
-    javaOptions.url = url;
+    destinationFile.writeTextSync('', function (e) {
+      throw e
+    })
+    const javaOptions = new org.nativescript.widgets.Async.Http.RequestOptions()
+    javaOptions.url = url
 
-    // allow methods other than GET
-
-    var javaUrl = new java.net.URL(url);
-    var connection = javaUrl.openConnection();
+    const javaUrl = new java.net.URL(url)
+    connection = javaUrl.openConnection()
     // allow optional headers to be sent
     if (options) {
-      const { headers, method } = options;
+      const { headers, method } = options
       if (method) {
         // this is GET by default
-        javaOptions.method = method;
-        connection.setRequestMethod(method);
+        javaOptions.method = method
+        connection.setRequestMethod(method)
       }
       if (headers) {
-        for (var key in headers) {
-          connection.setRequestProperty(key, headers[key]);
+        for (const key in headers) {
+          connection.setRequestProperty(key, headers[key])
         }
       }
     }
 
-    connection.connect();
-    if (connection.getResponseCode() != java.net.HttpURLConnection.HTTP_OK) {
-      throw "Server returned HTTP " + connection.getResponseCode();
+    connection.connect()
+    if (connection.getResponseCode() !== java.net.HttpURLConnection.HTTP_OK) {
+      throw new Error('Server returned HTTP ' + connection.getResponseCode())
     }
-    contentLength = connection.getContentLength();
 
-    var input = new java.io.BufferedInputStream(connection.getInputStream());
-    var output = new java.io.FileOutputStream(destinationFile.path);
+    contentLength = connection.getContentLength()
+    input = new java.io.BufferedInputStream(connection.getInputStream())
+    output = new java.io.FileOutputStream(destinationFile.path)
 
-    var data = Array.create("byte", 4096);
-    var total = 0;
-    var count = 0;
-    while ((count = input.read(data)) != -1) {
-      total += count;
-      output.write(data, 0, count);
+    const data = Array.create('byte', 4096)
+    let total = 0
+    let count = 0
+    while ((count = input.read(data)) !== -1) {
+      total += count
+      output.write(data, 0, count)
       if (contentLength > 0) {
-        var progress = ((100.0 / contentLength) * total) / 100;
-        global.postMessage({ progress: progress });
+        const progress = ((100.0 / contentLength) * total) / 100
+        global.postMessage({ progress: progress })
       }
     }
-    global.postMessage({ filePath: destinationFile.path });
+    global.postMessage({ filePath: destinationFile.path })
   } catch (ex) {
-    global.postMessage({ error: ex });
+    global.postMessage({ error: ex })
   } finally {
     if (output) {
-      output.flush();
-      output.close();
+      output.flush()
+      output.close()
     }
-    if (input) input.close();
-    if (connection) connection.disconnect();
+    if (input) {
+      input.close()
+    }
+    if (connection) {
+      connection.disconnect()
+    }
   }
-};
+}
 
 // does not handle errors with an `onerror` handler
 // errors will propagate directly to the main thread Worker instance
-
 // to handle errors implement the global.onerror handler:
 // global.onerror = function(err) {}
